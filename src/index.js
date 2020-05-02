76c2061a75a590ca5da0684e82ee517bec4f0a76
ryo-murai <murai.ry@gmail.com>
2020-05-02 11:06:36 +0900

    add handle click

--- src/index.js
+++ src/index.js
@@ -12,12 +12,7 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button
-        className="square"
-        onClick={() => {
-          this.setState({ value: "X" });
-        }}
-      >
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.state.value}
       </button>
     );
@@ -25,8 +20,24 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    alert("handleClick");
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
